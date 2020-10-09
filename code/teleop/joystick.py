d4429d3638108cd40b125f681e8caca43d767fa4
ViktorDanilin <Ewktor123>
2020-10-09 17:11:41 +0700

    commit

---
+++ code/teleop/joystick.py
@@ -0,0 +1,42 @@
+import pygame as pg
+import time
+pg.joystick.init()
+joystick_count = pg.joystick.get_count()
+
+if joystick_count == 0:
+    print("ERROR!")
+    exit()
+while True:
+    joystick = pg.joystick.Joystick(0)
+    joystick.init()
+
+    axes = joystick.get_numaxes()
+    axis = joystick.get_axis(4)
+    buttons = joystick.get_numbuttons()
+    button = joystick.get_button(1)
+    hats = joystick.get_numhats()
+    hat = joystick.get_hat(0)
+    print(axis)
+    time.sleep(0.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
