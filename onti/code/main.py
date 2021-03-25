bf8b1c9f67c6cb416888d6c0d41d4c7306b1db18
DanilinViktor <danilinvs58@gmail.com>
2021-03-25 21:56:46 +0700

    avtonomka

--- onti/code/main.py
+++ onti/code/main.py
@@ -6,8 +6,8 @@ from geometry_msgs.msg import Twist
 
 rospy.init_node('onti')
 pub_1 = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-
 global dist, button,x,z
+c = 1
 x = 0
 z = 0
 dist = 0
@@ -28,24 +28,25 @@ def start_cb(msg):
 
 sub_1 = rospy.Subscriber('/ultrasound', Range, range_cb)
 sub_2 = rospy.Subscriber('/pushed', Bool, start_cb)
-
-def main():
-    global dist, button,x,z
-    c = 0
+def start():
+    global dist, button, x, z
     if button:
         time.sleep(7)
-        c+=1
-        if c==1:
-            if dist>50 and dist!=0:
-                x = 0.5
-                z = 0
-                move(x,z)
-                time.sleep(2)
-        x = 0
-        z = 0
-        move(x,z)
-        print(dist, button)
+        if dist>50 or dist==0:
+            x = 0.5
+            z = 0
+            move(x,z)
+            time.sleep(2)
+            x = 0
+            z = 0
+            move(x, z)
+def main(c):
+    global dist, button,x,z
+    if c == 1:
+        start()
+        c += 1
+    print(dist, button)
 
 while not rospy.is_shutdown():
-    main()
+    main(c)
     rospy.sleep(0.1)
