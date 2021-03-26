795bd8d21dd87f9abc53009d316b435fce88bdb2
DanilinViktor <danilinvs58@gmail.com>
2021-03-26 13:40:17 +0700

    avtonomka

--- onti/code/odom.py
+++ onti/code/odom.py
@@ -1,29 +1,21 @@
 import rospy
-import time
 import math
 from math import atan, pi
 import tf
-from sensor_msgs.msg import Range
-from std_msgs.msg import Bool
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
-
 rospy.init_node('onti')
 pub_1 = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-global dist, button,x,z, x_pose, y_pose, angular
+
+global x,z, x_pose, y_pose, angular
 odom_xyt = (0, 0, 0)
 odom_0_xyt = None
 x_pose = float(raw_input())
 y_pose = float(raw_input())
-
 angular = 0.0
-c = 1
 x = 0
 z = 0
-dist = 0
-button = False
-
 
 def turn_around():
     global odom_xyt, odom_0_xyt, x_pose,y_pose, angular
@@ -98,14 +90,6 @@ def move(x,z):
     pub_1_vel.angular.z = z
     pub_1.publish(pub_1_vel)
 
-def range_cb(msg):
-    global dist
-    dist = msg.range
-
-def start_cb(msg):
-    global button
-    button = msg.data
-
 def odom_cb(mes):
     global odom_xyt, odom_0_xyt, odom_updated
     odom_yaw = tf.transformations.euler_from_quaternion([
@@ -115,40 +99,14 @@ mes.pose.pose.orientation.x, mes.pose.pose.orientation.y, mes.pose.pose.orientat
     odom_xyt = (mes.pose.pose.position.x-odom_0_xyt[0], mes.pose.pose.position.y-odom_0_xyt[1], (odom_yaw-odom_0_xyt[2]))
 
 
-sub_1 = rospy.Subscriber('/ultrasound', Range, range_cb)
-sub_2 = rospy.Subscriber('/pushed', Bool, start_cb)
-sub_3 = rospy.Subscriber('/odom', Odometry, odom_cb)
-def start():
-    global dist, button, x, z
-    while(True):
-        if button:
-            time.sleep(7)
-            if dist>50 or dist==0:
-                x = 0.5
-                z = 0
-                move(x,z)
-                time.sleep(2)
-                x = 0
-                z = 0
-                move(x, z)
-                break
+sub = rospy.Subscriber('/odom', Odometry, odom_cb)
+
+
 def main():
     global odom_xyt, odom_0_xyt
     turn_around()
     turn_forward()
-    # print(odom_xyt, " // ", odom_0_xyt)
 
 while not rospy.is_shutdown():
-    if c == 1:
-        start()
-        c += 1
-    else:
-        main()
+    main()
     rospy.sleep(0.1)
-
-
-# x-0.1
-# y0.4
-# (2, 2.896613990462929)
-# 0.412310562562
-# (1, -2.896613990462929)
